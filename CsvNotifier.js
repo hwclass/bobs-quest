63a13b070cd651beb61effc63576025de06be561
hwclass <harderworking.class@gmail.com>
2016-02-16 23:01:01 +0200

    created the path to saving the current csv data into redis instance, made it possible to notify and save the current data when changed

--- CsvNotifier.js
+++ CsvNotifier.js
@@ -6,13 +6,47 @@ var fs = require("fs"),
 			data : {
 				path : './data/',
 				fileName : 'sample.csv'
+			},
+			redis : {
+				port : 6379,
+				host : '127.0.0.1'
 			}
 		}
+
+var redis = require('redis'); 
+
+function connectRedis (port, host) {
+	return redis.createClient(port, host);
+}
+
+function setKey (redisClient, key, val) {
+	console.log('setKey');
+	redisClient.set(key, val, function(err, reply) {
+	  if (!err) {
+	  	console.log('key, ' + key + ' saved');
+	  } else {
+	  	console.dir(err);
+	  }
+	});
+}
+
+function getValue (redisClient, key) {
+	redisClient.get(key, function (err, reply) {
+		console.log('key : ' + reply);
+	});
+}
 
 fs.watchFile(config.data.path + config.data.fileName, function (event, fileName) {
 	var converterIns = new CsvToJsonConverter({});
 	fs.createReadStream(config.data.path + config.data.fileName).pipe(converterIns);
 	converterIns.on('end_parsed', function (jsonArr) {
 		console.dir(jsonArr);
+		redisClient = connectRedis(config.redis.port, config.redis.host);
+		console.dir(redisClient);
+		redisClient.on('connect', function() {
+			console.log('connected');
+		});
+		setKey(redisClient, 'founders', JSON.stringify(jsonArr));
+		getValue(redisClient, 'founders');
 	});
 });
