25abe64fabb177553d464ab5efc17f67075900b3
hwclass <harderworking.class@gmail.com>
2016-03-01 22:38:33 +0200

    changed the context of the map initializer file

--- client/js/modules/mapInitialization.js
+++ client/js/modules/mapInitialization.js
@@ -5,21 +5,25 @@
 
 var MapInitialization = (function () {
 
-  function initMap (latitude, longitude) {
-    var mapDiv = document.getElementById('map');
-    setMapLoadingStatus(true);
-    var map = cachedMap = new google.maps.Map(mapDiv, {
-      center: {lat: 44.540, lng: -78.546},
-      zoom: 8
-    });
-  }
-  
-  function setMapLoadingStatus (state) {
-    mapLoaded = state;
+  var publicApi = {
+
+    initMap : function (latitude, longitude) {
+      var mapDiv = document.getElementById('map');
+      publicApi.setMapLoadingStatus(true);
+      var map = cachedMap = new google.maps.Map(mapDiv, {
+        center: {lat: 44.540, lng: -78.546},
+        zoom: 8
+      });
+    },
+    
+    setMapLoadingStatus : function (state) {
+      mapLoaded = state;
+    }
+
   }
 
   return {
-    init : initMap
+    init : publicApi.initMap
   }
 
 }());
